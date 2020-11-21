d3a2424dd5560fd5163c2b5a660f1592a494df95
jonatronblah <jonatron@gmail.com>
2020-11-21 16:16:44 -0500

    token auth in process

--- services/web/app/__init__.py
+++ services/web/app/__init__.py
@@ -1,11 +1,18 @@
-from fastapi import FastAPI, Request
+from fastapi import Depends, FastAPI, Request
+from fastapi.security import OAuth2PasswordBearer 
 
 from app.routes.message import router as MessageRouter
 
 
 app = FastAPI()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-app.include_router(MessageRouter, tags=["Message"], prefix="/message")
+
+app.include_router(
+MessageRouter, 
+tags=["Message"], 
+prefix="/message",
+dependencies=[Depends(oauth2_scheme)])
 
 
 
