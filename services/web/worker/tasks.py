1e108049b55b0178898511e77d97bfba0b23678e
unknown <jonatronblah@github.com>
2020-11-23 16:09:09 -0500

    celery beat scheduler added

--- services/web/worker/tasks.py
+++ services/web/worker/tasks.py
@@ -2,5 +2,16 @@ from worker.celery_app import celery_app
 
 
 @celery_app.task
-def my_task():
-    pass
+def my_task(msg):
+    print(msg)
+  
+msg = "hello there!"
+  
+celery_app.conf.beat_schedule = {
+    # test to send email every min
+    'test-schedule': {
+        'task': 'worker.tasks.my_task',
+        'schedule': 10.0,
+        'args': ([msg])
+    },
+}
